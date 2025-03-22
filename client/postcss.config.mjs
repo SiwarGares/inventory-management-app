7ae4c09d7b6adaa0029cd5103693784377a468ca
unknown <https://github.com/SiwarGares/gmc_learn_git.git>
2025-03-22 21:29:03 +0100

    navbar and sidebar added

---
+++ client/postcss.config.mjs
@@ -0,0 +1,22 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+
+
+
+
+
+
+
+
+
+/* const config = {
+  plugins: ["@tailwindcss/postcss"],
+};
+
+export default config;
+ */
